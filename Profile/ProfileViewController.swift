6261a4928a836c9a703f20f6f35032fa3f57d237
S1mbach <simbatron999@gmail.com>
2024-06-04 23:17:19 +0800

    Down

--- Profile/ProfileViewController.swift
+++ Profile/ProfileViewController.swift
@@ -2,9 +2,17 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    let headerView = ProfileHeaderView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        self.title = "Profile"
+        view.backgroundColor = .lightGray
+        view.addSubview(headerView)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        headerView.frame = view.bounds
     }
 }
+
